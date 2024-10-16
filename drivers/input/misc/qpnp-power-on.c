97bfb38e9a54507ec53a5fa96c0bd34cca422e71
寒い-Scent <shenameru@gmail.com>
2024-10-16 11:55:17 +0000

    input: qpnp-power-on: Import xiaomi changes
    Co-authored-by: shenprjkt <shenprjktplayground@gmail.com>

--- drivers/input/misc/qpnp-power-on.c
+++ drivers/input/misc/qpnp-power-on.c
@@ -35,6 +35,8 @@
 #include <linux/qpnp/qpnp-misc.h>
 #include <linux/power_supply.h>
 
+#include <asm/bootinfo.h>
+
 #define PMIC_VER_8941           0x01
 #define PMIC_VERSION_REG        0x0105
 #define PMIC_VERSION_REV4_REG   0x0103
@@ -68,6 +70,7 @@
 	((pon)->base + PON_OFFSET((pon)->subtype, 0xA, 0xC2))
 #define QPNP_POFF_REASON1(pon) \
 	((pon)->base + PON_OFFSET((pon)->subtype, 0xC, 0xC5))
+#define QPNP_POFF_REASON2(pon)                  ((pon)->base + 0xD)
 #define QPNP_PON_WARM_RESET_REASON2(pon)	((pon)->base + 0xB)
 #define QPNP_PON_OFF_REASON(pon)		((pon)->base + 0xC7)
 #define QPNP_FAULT_REASON1(pon)			((pon)->base + 0xC8)
@@ -360,6 +363,8 @@ int qpnp_pon_set_restart_reason(enum pon_restart_reason reason)
 	if (!pon->store_hard_reset_reason)
 		return 0;
 
+	pr_err("pon_restart_reason=0x%x\n", reason);
+
 	if (is_pon_gen2(pon) && !pon->legacy_hard_reset_offset)
 		rc = qpnp_pon_masked_write(pon, QPNP_PON_SOFT_RB_SPARE(pon),
 					   GENMASK(7, 1), (reason << 1));
@@ -501,6 +506,45 @@ qpnp_get_cfg(struct qpnp_pon *pon, u32 pon_type)
 }
 
 static DEVICE_ATTR(debounce_us, 0664, qpnp_pon_dbc_show, qpnp_pon_dbc_store);
+
+static ssize_t qpnp_kpdpwr_reset_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct qpnp_pon *pon = dev_get_drvdata(dev);
+	u8 val;
+	int rc;
+	rc = spmi_ext_register_readl(pon->spmi->ctrl, pon->spmi->sid, QPNP_PON_S3_SRC(pon), &val, 1);
+	if (rc) {
+		pr_err("Unable to read pon_dbc_ctl rc=%d\n", rc);
+		return rc;
+	}
+	rc = spmi_ext_register_readl(pon->spmi->ctrl, pon->spmi->sid, QPNP_PON_KPDPWR_S2_CNTL2(pon), &val, 1);
+	if (rc) {
+		pr_err("Unable to read pon_dbc_ctl rc=%d\n", rc);
+		return rc;
+	}
+	val &= QPNP_PON_S2_RESET_ENABLE;
+	val = val >> 7;
+	return snprintf(buf, QPNP_PON_BUFFER_SIZE, "%d\n", val);
+}
+static ssize_t qpnp_kpdpwr_reset_store(struct device *dev,
+				struct device_attribute *attr,
+				const char *buf, size_t size)
+{
+	struct qpnp_pon *pon = dev_get_drvdata(dev);
+	u32 value;
+	int rc;
+	if (size > QPNP_PON_BUFFER_SIZE)
+		return -EINVAL;
+	rc = kstrtou32(buf, 10, &value);
+	if (rc)
+		return rc;
+	value = value << 7;
+	rc = qpnp_pon_masked_write(pon, QPNP_PON_KPDPWR_S2_CNTL2(pon), QPNP_PON_S2_RESET_ENABLE, value);
+	return size;
+}
+static DEVICE_ATTR(kpdpwr_reset, 0664, qpnp_kpdpwr_reset_show, qpnp_kpdpwr_reset_store);
+
 
 #define PON_TWM_ENTRY_PBS_BIT           BIT(0)
 static int qpnp_pon_reset_config(struct qpnp_pon *pon,
@@ -777,6 +821,107 @@ int qpnp_pon_is_warm_reset(void)
 		return pon->warm_reset_reason1;
 }
 EXPORT_SYMBOL(qpnp_pon_is_warm_reset);
+
+int qpnp_pon_is_ps_hold_reset(void)
+{
+	struct qpnp_pon *pon = sys_reset_dev;
+	int rc;
+	int reg = 0;
+	u8 buf[2];
+	if (!pon)
+		return 0;
+#if 0
+	rc = regmap_read(pon->regmap, QPNP_POFF_REASON1(pon), &reg);
+	if (rc) {
+		dev_err(&pon->spmi->dev,
+				"Unable to read addr=%x, rc(%d)\n",
+				QPNP_POFF_REASON1(pon), rc);
+		return 0;
+	}
+#endif
+		rc = spmi_ext_register_readl(pon->spmi->ctrl, pon->spmi->sid,
+						QPNP_POFF_REASON1(pon),
+						buf, 1);
+		if (rc) {
+			dev_err(&pon->spmi->dev, "Unable to read POFF_REASON1 reg rc:%d\n",
+				rc);
+			return rc;
+		}
+	dev_info(&pon->spmi->dev, "QPNP_POFF_REASON1 buf[0]=%x buf[1]=%x\n", buf[0], buf[1]);
+	/*The bit 1 is 1, means by PS_HOLD/MSM controlled shutdown */
+	if (buf[0] & (1<<POFF_REASON_EVENT_PS_HOLD))
+		return 1;
+#if 0
+	dev_info(&pon->spmi->dev,
+			"hw_reset reason1 is 0x%x\n",
+			reg);
+	rc = regmap_read(pon->regmap, QPNP_POFF_REASON2(pon), &reg);
+#endif
+		rc = spmi_ext_register_readl(pon->spmi->ctrl, pon->spmi->sid,
+						QPNP_POFF_REASON2(pon),
+						buf, 1);
+		if (rc) {
+			dev_err(&pon->spmi->dev, "Unable to read POFF_REASON1 reg rc:%d\n",
+				rc);
+			return rc;
+		}
+	dev_info(&pon->spmi->dev, "QPNP_POFF_REASON2 buf[0]=%x buf[1]=%x\n", buf[0], buf[1]);
+	dev_info(&pon->spmi->dev,
+			"hw_reset reason2 is 0x%x\n",
+			reg);
+	return 0;
+}
+EXPORT_SYMBOL(qpnp_pon_is_ps_hold_reset);
+int qpnp_pon_is_lpk(void)
+{
+	struct qpnp_pon *pon = sys_reset_dev;
+	int rc;
+	int reg = 0;
+	u8 buf[2];
+	if (!pon)
+		return 0;
+#if 0
+	rc = regmap_read(pon->regmap, QPNP_POFF_REASON1(pon), &reg);
+	if (rc) {
+		dev_err(&pon->spmi->dev,
+				"Unable to read addr=%x, rc(%d)\n",
+				QPNP_POFF_REASON1(pon), rc);
+		return 0;
+	}
+#endif
+	rc = spmi_ext_register_readl(pon->spmi->ctrl, pon->spmi->sid,
+			QPNP_POFF_REASON1(pon),
+			buf, 1);
+	if (rc) {
+		dev_err(&pon->spmi->dev, "Unable to read POFF_REASON1 reg rc:%d\n",
+				rc);
+		return rc;
+	}
+	dev_info(&pon->spmi->dev, "QPNP_POFF_REASON1 buf[0]=%x buf[1]=%x\n", buf[0], buf[1]);
+	/* The bit 7 is 1, means the off reason is powerkey */
+	if (buf[0] & (1<<POFF_REASON_EVENT_KPDPWR_N))
+		return 1;
+	dev_info(&pon->spmi->dev,
+			"hw_reset reason1 is 0x%x\n",
+			reg);
+#if 0
+	rc = regmap_read(pon->regmap, QPNP_POFF_REASON2(pon), &reg);
+#endif
+	rc = spmi_ext_register_readl(pon->spmi->ctrl, pon->spmi->sid,
+			QPNP_POFF_REASON2(pon),
+			buf, 1);
+	if (rc) {
+		dev_err(&pon->spmi->dev, "Unable to read POFF_REASON1 reg rc:%d\n",
+				rc);
+		return rc;
+	}
+	dev_info(&pon->spmi->dev, "QPNP_POFF_REASON2 buf[0]=%x buf[1]=%x\n", buf[0], buf[1]);
+	dev_info(&pon->spmi->dev,
+			"hw_reset reason2 is 0x%x\n",
+			reg);
+	return 0;
+}
+EXPORT_SYMBOL(qpnp_pon_is_lpk);
 
 /**
  * qpnp_pon_wd_config - Disable the wd in a warm reset.
